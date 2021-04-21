1bb85bc53cc744d4bda13e027a40ff364267a5cc
FilipKowalski195 <42491472+FilipKowalski195@users.noreply.github.com>
2021-04-21 20:45:42 +0200

    Changes normal table to Datagrit

--- src/reposList.js
+++ src/reposList.js
@@ -1,18 +1,22 @@
+import { useMemo } from 'react'
 import { selectRepositories, fetchRepositories } from './features/repositories/repositoriesSlice'
 import { useDispatch, useSelector, } from 'react-redux'
 import { useEffect } from 'react'
-import { 
-  Paper,
-  Table,
-  TableContainer, 
-  TableCell, 
-  TableHead, 
-  TableBody, 
-  TableRow 
-} from '@material-ui/core'
+import { DataGrid } from '@material-ui/data-grid';
 
 
 export const ReposList = (props) => {
+
+  const columns = [
+
+    { field: 'id', headerName: 'ID', width: 70, hide: true},
+    { field: 'name', headerName: 'Repository name', width: 180 },
+    { field: 'stars', headerName: 'Stars no.', type: 'number', width: 150 },
+    { field: 'forks', headerName: 'Forks no.', type: 'number', width: 150 },
+    { field: 'lang', headerName: 'Programming language', width: 250 },
+    
+  ];
+  
     const dispatch = useDispatch()
     const repos = useSelector(selectRepositories)
  
@@ -22,31 +26,19 @@ export const ReposList = (props) => {
         }
       }, [props.name, dispatch])
   
-    return (
-      
-      <TableContainer component={Paper}>
-      <Table aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>Name</TableCell>
-            <TableCell align="center">Stars</TableCell>
-            <TableCell align="center">Forks</TableCell>
-            <TableCell align="center">Programming language</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {repos.slice().sort((a, b) => a.stargazers_count < b.stargazers_count ? 1 : -1).map((row) => (
-            <TableRow key={row.id}>
-              <TableCell component="th" scope="row">
-                {row.name}
-              </TableCell>
-              <TableCell align="center">{row.stargazers_count}</TableCell>
-              <TableCell align="center">{row.forks}</TableCell>
-              <TableCell align="center">{row.language}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer> 
-    )
+    const rows = useMemo(() =>  repos.map((data) => { 
+      return {
+        id: data.id,
+        name: data.name, 
+        stars: data.stargazers_count,
+        forks: data.forks,
+        lang: data.language
+      }
+    }), [repos])
+
+      return (
+        <div style={{ height: 500, width: '100%' }}>
+          <DataGrid id="gridContainer" rows={rows} columns={columns} pageSize={7}/>
+        </div>
+      );
   }
